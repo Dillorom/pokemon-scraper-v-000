a55c6735aa6bed51c85d47569b256ba1c9b77b2f
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-20 22:19:47 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -25,4 +25,9 @@ class Pokemon
     selected_pokemon = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id).flatten
       Pokemon.new(id: selected_pokemon[0], name: selected_pokemon[1], type: selected_pokemon[2], db:db)
   end
+
+  #bonus
+  def alter_hp(hp)
+    db.execute("UPDATE pokemon SET hp = ?", new_hp, self.id)
+  end
 end
